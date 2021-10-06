02c9165a3e0c9c14e426c07b42a1597db78ac7d0
Michael L. Collard <collard@uakron.edu>
2021-10-06 17:30:53 -0400

    Starting parser

---
+++ parser.cpp
@@ -0,0 +1,28 @@
+/*
+    identity.cpp
+
+    Identity copy
+*/
+
+#include <iostream>
+#include <fstream>
+#include "Language.h"
+
+int main(int argc, char* argv[]) {
+
+    // setup input
+    std::ifstream file(argv[1]);
+    antlr4::ANTLRInputStream input(file);
+
+    // setup lexer
+    Language lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+
+    // run lexer
+    tokens.fill();
+    for (auto token : tokens.getTokens()) {
+        std::cout << token->getText();
+    }
+
+    return 0;
+}
