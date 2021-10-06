047f3f3914c4f8712edc59df21409a81a311dc65
Michael L. Collard <collard@uakron.edu>
2021-10-06 18:07:55 -0400

    Async for and async def

--- parser.cpp
+++ parser.cpp
@@ -8,6 +8,79 @@
 #include <fstream>
 #include "Language.h"
 
+// for
+void parseFor(antlr4::BufferedTokenStream& stream) {
+
+   if (stream.LA(1) == Language::FOR) {
+        std::cout << stream.LT(1)->toString();
+        stream.consume();
+    } else {
+        std::cerr << "ERROR\n";
+        exit(1);
+    }
+}
+
+// def
+void parseDef(antlr4::BufferedTokenStream& stream) {
+
+   if (stream.LA(1) == Language::DEF) {
+        std::cout << stream.LT(1)->toString();
+        stream.consume();
+    } else {
+        std::cerr << "ERROR\n";
+        exit(1);
+    }
+}
+
+// return
+void parseReturn(antlr4::BufferedTokenStream& stream) {
+
+   if (stream.LA(1) == Language::RETURN) {
+        std::cout << stream.LT(1)->toString();
+        stream.consume();
+    } else {
+        std::cerr << "ERROR\n";
+        exit(1);
+    }
+}
+
+// async (def | for)?
+void parseAsync(antlr4::BufferedTokenStream& stream) {
+
+    // async
+    if (stream.LA(1) == Language::ASYNC) {
+        std::cout << stream.LT(1)->toString();
+        stream.consume();
+    } else {
+        std::cerr << "ERROR\n";
+        exit(1);
+    }
+
+    // (def | for)?
+    if (stream.LA(1) == Language::DEF) {
+        parseDef(stream);
+    } else if (stream.LA(1) == Language::FOR) {
+        parseFor(stream);
+    }
+}
+
+// (def | return | for | async)
+void parseStart(antlr4::BufferedTokenStream& stream) {
+
+    if (stream.LA(1) == Language::DEF) {
+        parseDef(stream);
+    } else if (stream.LA(1) == Language::RETURN) {
+        parseReturn(stream);
+    } else if (stream.LA(1) == Language::FOR) {
+        parseFor(stream);
+    } else if (stream.LA(1) == Language::ASYNC) {
+        parseAsync(stream);
+    } else {
+        std::cerr << "ERROR\n";
+        exit(1);
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     // setup input
@@ -24,7 +97,8 @@ int main(int argc, char* argv[]) {
         if (tokens.LA(1) == Language::EOF)
             break;
 
-        tokens.consume();
+        parseStart(tokens);
+        std::cout << '\n';
     }
 
     return 0;
